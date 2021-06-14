34f035534bf76b92958f6d8dc09f0335aa485f95
develiberta <develiberta@gmail.com>
2021-06-14 21:44:11 +0900

    step_01_04

---
+++ baekjoon/src/step/_01_IOAndOperations/_04_10172_Dog.java
@@ -0,0 +1,24 @@
+package step._01_IOAndOperations;
+
+/* date : 2021-06-14 (월)
+ * author : develiberta
+ * [문제]
+ * 아래 예제와 같이 개를 출력하시오.
+ * [입력]
+ * 없음
+ * [출력]
+ * |\_/|
+ * |q p|   /}
+ * ( 0 )"""\
+ * |"^"`    |
+ * ||_/=\\__|
+ */
+public class _04_10172_Dog {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
