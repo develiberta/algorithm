61813ee5a755fa321105e610fc79b1efc9c49197
develiberta <develiberta@gmail.com>
2021-06-16 21:36:42 +0900

    step_01_08

---
+++ baekjoon/src/step/_01_IOAndOperations/_06_01001_AMinusB.java
@@ -0,0 +1,26 @@
+package step._01_IOAndOperations;
+
+import java.util.Scanner;
+
+/* date : 2021-06-16 (수)
+ * author : develiberta
+ *
+ * [문제]
+ * 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+ * [입력]
+ * 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ * [출력]
+ * 첫째 줄에 A-B를 출력한다.
+ * (예제 입력 1)
+ * 3 2
+ * (예제 출력 1)
+ * 1
+ */
+public class _06_01001_AMinusB {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        System.out.print(A-B);
+    }
+}
---
+++ baekjoon/src/step/_01_IOAndOperations/_08_01008_ADividedByB.java
@@ -0,0 +1,30 @@
+package step._01_IOAndOperations;
+
+import java.util.Scanner;
+
+/* date : 2021-06-16 (수)
+ * author : develiberta
+ *
+ * [문제]
+ * 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+ * [입력]
+ * 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ * [출력]
+ * 첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10E-9 이하이면 정답이다.
+ * (예제 입력 1)
+ * 1 3
+ * (예제 출력 1)
+ * 0.33333333333333333333333333333333
+ * (예제 입력 2)
+ * 4 5
+ * (예제 출력 2)
+ * 0.8
+ */
+public class _08_01008_ADividedByB {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        System.out.print((double)A/B);
+    }
+}
